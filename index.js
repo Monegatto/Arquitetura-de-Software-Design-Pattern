17faf9f24a3ad923323d14aea89cda91f46039f9
giovaniohira <giovanirossi863@gmail.com>
2024-04-07 18:43:40 -0300

    feat: adição da CLI

--- index.js
+++ index.js
@@ -1,20 +1,102 @@
+import readline from 'readline';
+import GerenciadorContatos from './GerenciadorContatos.js';
 import Contato from './Contato.js';
-import GerenciadorContatos from "./GerenciadorContatos.js";
 
-const gerenciador = new GerenciadorContatos();
+class CLI {
+    constructor() {
+        this.gerenciadorContatos = new GerenciadorContatos();
+        this.rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    }
 
-let contato = new Contato("Joao", "123", "jp.net",)
-let contato2 = new Contato("Pedro", "1234", "pedro.net",)
-let contato3 = new Contato("Boter", "1235", "boter.net",)
-let contato1 = new Contato("Joao2", "123", "jp2.net")
+    exibirMenu() {
+        console.log('=== Menu ===');
+        console.log('1. Adicionar Contato');
+        console.log('2. Remover Contato');
+        console.log('3. Listar Contatos');
+        console.log('4. Buscar Contato');
+        console.log('5. Sair');
+    }
 
-gerenciador.adicionarContatos(contato);
-gerenciador.adicionarContatos(contato2);
-gerenciador.adicionarContatos(contato3);
-gerenciador.adicionarContatos(contato1)
+    adicionarContato() {
+        this.rl.question('Nome: ', nome => {
+            this.rl.question('Telefone: ', telefone => {
+                this.rl.question('Email: ', email => {
+                    const contato = new Contato(nome, telefone, email);
+                    this.gerenciadorContatos.adicionarContato(contato);
+                    console.log('Contato adicionado com sucesso!');
+                    this.menu();
+                });
+            });
+        });
+    }
 
+    removerContato() {
+        this.rl.question('Telefone do Contato a ser removido: ', telefone => {
+            if (this.gerenciadorContatos.removerContato(telefone)) {
+                console.log('Contato removido com sucesso!');
+            } else {
+                console.log('Contato não encontrado.');
+            }
+            this.menu();
+        });
+    }
 
+    listarContatos() {
+        const contatos = this.gerenciadorContatos.listarContatos();
+        console.log('=== Contatos ===');
+        contatos.forEach(contato => {
+            console.log(`Nome: ${contato.nome}, Telefone: ${contato.telefone}, Email: ${contato.email}`);
+        });
+        this.menu();
+    }
 
-for(let i = 0; i < gerenciador.listaContatos.length; i++){
-    console.log(gerenciador.listaContatos[i]);
+    buscarContato() {
+        this.rl.question('Telefone do Contato a ser buscado: ', telefone => {
+            const contato = this.gerenciadorContatos.buscarContato(telefone);
+            if (contato) {
+                console.log(`Nome: ${contato.nome}, Telefone: ${contato.telefone}, Email: ${contato.email}`);
+            } else {
+                console.log('Contato não encontrado.');
+            }
+            this.menu();
+        });
+    }
+
+    menu() {
+        this.exibirMenu();
+        this.rl.question('Escolha uma opção: ', opcao => {
+            switch (opcao) {
+                case '1':
+                    this.adicionarContato();
+                    break;
+                case '2':
+                    this.removerContato();
+                    break;
+                case '3':
+                    this.listarContatos();
+                    break;
+                case '4':
+                    this.buscarContato();
+                    break;
+                case '5':
+                    console.log('Saindo...');
+                    this.rl.close();
+                    break;
+                default:
+                    console.log('Opção inválida.');
+                    this.menu();
+            }
+        });
+    }
+
+    iniciar() {
+        console.log('Bem-vindo ao sistema de gerenciamento de contatos!');
+        this.menu();
+    }
 }
+
+const cli = new CLI();
+cli.iniciar();
